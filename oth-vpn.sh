43614bdd3c1bb065afaa9ff60ea80b71a0ce36b6
johannesroesner <roesnerj@icloud.com>
2025-10-06 18:30:39 +0200

    fix: oth-vpn.sh works now with switching trusted-cert

--- oth-vpn.sh
+++ oth-vpn.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
-sudo openfortivpn -c ~/.openfortivpn/config
+OUTPUT=$(sudo openfortivpn -c ~/.openfortivpn/config 2>&1)
+TRUSTED_CERT=$(echo "$OUTPUT" | grep 'trusted-cert' | grep -o '[a-f0-9]\{64\}' | head -n 1)
+sudo openfortivpn -c ~/.openfortivpn/config --trusted-cert $TRUSTED_CERT
